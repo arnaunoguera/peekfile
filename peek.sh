897c54ed9d54dec69918f2a6b5dd7c3830fa1945
Arnau <arnoguera724@gmail.com>
2023-10-29 11:41:51 +0100

    third commit

--- peek.sh
+++ peek.sh
@@ -1,3 +1,15 @@
-head -n $2 $1
-echo ...
-tail -n $2 $1
+if [[ $# -eq 1 ]]
+then
+    lines=3
+else
+    lines=$2
+fi
+if [[ $(cat $1 | wc -l) -le $((2*$lines)) ]]
+then 
+    cat $1
+else
+    echo "Warning: the file is too long. Showing first and last $lines lines." >&2
+    head -n $lines $1
+    echo ...
+    tail -n $lines $1
+fi
