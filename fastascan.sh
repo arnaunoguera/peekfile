0307b1bac6877b6bc54dfb7814ffe43af7704439
Arnau <arnoguera724@gmail.com>
2023-11-11 22:43:59 +0100

    Updated fastascan

--- fastascan.sh
+++ fastascan.sh
@@ -26,13 +26,20 @@ files=$(find $folder -type f -name "*.fa" -or -type l -name "*.fa" -or -type f -
 
 # Calculating the number of files
 Nfiles=$(echo "$files" | wc -l)
-# Echoing $files quoted separates each result into a newline again
+# Echoing $files in quotes separates each result into a newline again
+# It also makes Nfiles = 1 if it's actually 0, so I have to consider this case. 
+if [[ $(echo $files | wc -w) -eq 0 ]]
+then
+    echo There are no .fasta or .fa files in the folder $folder or subfolders.
+    echo
+    exit 1 #I stop the program if there are no fasta/fa files
+fi
 
 # Calculating the amount of unique IDs 
 NIDs=$( (echo "$files" | while read i; do cat "$i"; done) | awk '/^>/{print $1}' | sort | uniq | wc -l)
 # Only the string from '>' until the first space is considered the ID
 
-#Printing the folder summary
+# Printing the folder summary
 echo '##################################  SUMMARY  ##################################'
 echo
 echo There are $Nfiles fasta/fa files in the folder and its subfolders.
